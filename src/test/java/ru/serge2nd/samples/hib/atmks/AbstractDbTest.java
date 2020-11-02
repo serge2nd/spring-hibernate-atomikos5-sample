56c24edb0e915b2184b08cf639fc7b2718bfe85f
Sergey Pistyulga <serge2nd@yandex.ru>
2020-11-02 18:40:07 +0300

    Refactor tests

--- src/test/java/ru/serge2nd/samples/hib/atmks/AbstractDbTest.java
+++ src/test/java/ru/serge2nd/samples/hib/atmks/AbstractDbTest.java
@@ -9,11 +9,16 @@ import ru.serge2nd.samples.hib.atmks.data.RouteStop;
 import ru.serge2nd.samples.hib.atmks.data.Station;
 import ru.serge2nd.samples.hib.atmks.data.StayInterval;
 
-import javax.persistence.LockModeType;
 import java.time.LocalTime;
-import java.util.stream.Stream;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static java.util.Comparator.comparingInt;
+import static javax.persistence.LockModeType.OPTIMISTIC_FORCE_INCREMENT;
+import static org.hamcrest.CoreMatchers.nullValue;
+import static org.junit.jupiter.api.Assertions.fail;
+import static ru.serge2nd.test.matcher.ArrayMatch.isArray;
+import static ru.serge2nd.test.matcher.AssertThat.assertThat;
+import static ru.serge2nd.test.matcher.CommonMatch.equalTo;
+import static ru.serge2nd.test.matcher.CommonMatch.fails;
 
 abstract class AbstractDbTest {
     @Autowired TxSessionTemplate t;
@@ -30,14 +35,20 @@ abstract class AbstractDbTest {
                 .setParameter("id", 200L)
                 .setReadOnly(true)
                 .getSingleResult());
+        Object[] routeStops = route.routeStops().sorted(comparingInt(RouteStop::getIndexNumber)).toArray();
 
-        /* THEN */ assertAll(() ->
-        assertEquals(2, route.routeStops().count()), () ->
-        assertAll(route.routeStops().flatMap(rs -> Stream.of(() ->
-        assertSame(route, rs.getRoute()), () -> {
-        assertNotNull(rs.getStation());
-        assertNotNull(rs.getStation().getName());
-        }))));
+        /* THEN */ assertThat(
+        routeStops, isArray(
+                RouteStop.builder()
+                        .indexNumber(50)
+                        .station(Station.builder().name("ЛЕСНАЯ").build())
+                        .stayInterval(new StayInterval(LocalTime.of(6, 30), LocalTime.of(6, 32)))
+                        .build(),
+                RouteStop.builder()
+                        .indexNumber(100)
+                        .station(Station.builder().name("МОСКВА КИЕВСКАЯ").build())
+                        .stayInterval(new StayInterval(LocalTime.of(7, 45), LocalTime.of(7, 45)))
+                        .build()));
     }
 
     @Test
@@ -49,10 +60,7 @@ abstract class AbstractDbTest {
                         .routeStop(RouteStop.builder()
                                 .indexNumber(111)
                                 .station((Station)session.merge(Station.builder().id(90L).name("abc").build()))
-                                .stayInterval(StayInterval.builder()
-                                        .arrivalOn(LocalTime.of(8, 27))
-                                        .departureOn(LocalTime.of(8, 28))
-                                        .build())
+                                .stayInterval(new StayInterval(LocalTime.of(8, 27), LocalTime.of(8, 28)))
                                 .build())
                         .build()));
 
@@ -64,20 +72,15 @@ abstract class AbstractDbTest {
                 .setParameter("id", id)
                 .setReadOnly(true)
                 .getSingleResult());
+        RouteStop[] routeStops = route.routeStops().toArray(RouteStop[]::new);
 
-        /* THEN */ assertAll(() ->
-        assertEquals(1, route.routeStops().count()), () ->
-        // AND
-        route.routeStops().findFirst().ifPresent(rs -> assertAll(() ->
-        assertEquals(111, rs.getIndexNumber()), () -> {
-        // AND
-        assertNotNull(rs.getStation()); assertAll(() ->
-        assertEquals(90L, rs.getStation().getId()), () ->
-        assertEquals("abc", rs.getStation().getName())); }, () -> {
-        // AND
-        assertNotNull(rs.getStayInterval()); assertAll(() ->
-        assertEquals(LocalTime.of(8, 27), rs.getStayInterval().getArrivalOn()), () ->
-        assertEquals(LocalTime.of(8, 28), rs.getStayInterval().getDepartureOn())); })));
+        /* THEN */ assertThat(
+        routeStops                        , isArray(RouteStop.builder()
+                                                .indexNumber(111)
+                                                .station(Station.builder().name("abc").build())
+                                                .stayInterval(new StayInterval(LocalTime.of(8, 27), LocalTime.of(8, 28)))
+                                                .build()),
+        routeStops[0].getStation().getId(), equalTo(90L));
     }
 
     @Test
@@ -85,7 +88,7 @@ abstract class AbstractDbTest {
         // GIVEN
         Route route = Route.builder().build();
         t.withTxSession((session, tx) -> session.save(route));
-        t.withTxSession((session, tx) -> session.find(Route.class, route.getId(), LockModeType.OPTIMISTIC_FORCE_INCREMENT));
+        t.withTxSession((session, tx) -> session.find(Route.class, route.getId(), OPTIMISTIC_FORCE_INCREMENT));
 
         // WHEN
         route.addRouteStop(RouteStop.builder()
@@ -98,7 +101,7 @@ abstract class AbstractDbTest {
                 .build());
 
         // THEN
-        assertThrows(HibernateOptimisticLockingFailureException.class, () -> t.withTxSession(((session, tx) -> session.merge(route))));
+        assertThat(()->t.withTxSession(((session, tx)->session.merge(route))), fails(HibernateOptimisticLockingFailureException.class));
     }
 
     @Test
@@ -111,7 +114,7 @@ abstract class AbstractDbTest {
         }); } catch(Exception e) { e.printStackTrace(); }
 
         // THEN
-        assertNull(t.fromTxSession((session, tx) -> session.find(Station.class, id)));
+        assertThat(t.fromTxSession((session, tx)->session.find(Station.class, id)), nullValue());
     }
 
     static void sleep(long ms) {try {Thread.sleep(ms);} catch(InterruptedException e) {fail("unexpected interruption", e);}}
